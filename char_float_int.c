33f3c11ebbea3df7c70e68f3007535fac8dcf45f
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 12:15:32 +0530

    Add files via upload

---
+++ char_float_int.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	
+	int x=89;
+	printf("\n size of variable of x is %d",sizeof(x));
+	printf("\n size of variable of integer data type %d",sizeof(int));
+    printf("\nsize of variable of char data type %d",sizeof(char));	
+    printf("\n size of variable of float data type %d",sizeof(float));
+    return 0;
+}
+
+
